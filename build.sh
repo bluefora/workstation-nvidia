d356fc840de73548732c7ce31a5cedf330e3f9ad
Derixithy <derixithy@users.noreply.github.com>
2025-02-27 11:37:22 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -31,8 +31,6 @@ dnf -y swap ffmpeg-free ffmpeg --allowerasing
 # Setup flatpak apps
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-modify --no-filter --enable flathub
-APPS=$(flatpak list --app-runtime=org.fedoraproject.Platform --columns=application | tail -n +1 )
-flatpak install -y --reinstall flathub $APPS
 flatpak remote-delete fedora
 flatpak install -y com.mojang.Minecraft \
                   org.mozilla.firefox \
