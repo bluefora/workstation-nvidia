59ab04a07764b27cafc4b2f624e609110f4a9134
Derixithy <derixithy@users.noreply.github.com>
2025-02-27 13:07:19 +0100

    Update build.sh

--- build.sh
+++ build.sh
@@ -28,6 +28,8 @@ dnf -y install gnome-shell-extension-appindicator \
 # Install codecs
 dnf -y swap ffmpeg-free ffmpeg --allowerasing
 
+systemctl enable yafti.service
+
 # Setup flatpak apps
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
 flatpak remote-modify --no-filter --enable flathub
